9950238c548f6f9f4da869e535f8e662e7d65e51
Gabriel Gevert <gevertlolz@gmail.com>
2023-12-07 23:38:44 -0300

    Adição das páginas e rotas + criação  cookie

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  App
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
+  </nav>
+  <RouterView></RouterView>
 </template>
 
 <script setup>
-
+import { RouterView, RouterLink } from 'vue-router';
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import router from './router/index.js'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.withCredentials = true
+
+createApp(App).use(router).mount('#app')
